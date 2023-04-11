565d30d3d4b1238df3d4ce06b875ca322a453fc1
Anton Popov <a.popov.fizteh@gmail.com>
2023-04-11 14:54:35 +0300

    Functional data vizualization

--- PA_2D_scan_vizualization.py
+++ PA_2D_scan_vizualization.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = 'Sample_name-TiN-Full1.npy'
+filename = 'measuring results/Sample_name-TiN-Full4.npy'
 
 class IndexTracker:
-    def __init__(self, ax, data):
+    def __init__(self, fig, ax, data):
         self.ax = ax
+        self.fig = fig
         ax.set_title('Photoacoustic signal')
         ax.set_xlabel('us')
         ax.set_ylabel('V')
@@ -14,39 +15,42 @@ class IndexTracker:
         self.y_max = data.shape[0]
         self.x_ind = 0
         self.y_ind = 0
-
-        self.im = ax.plot(self.data[self.x_ind,self.y_ind,:])
         self.update()
 
     def on_key_press(self, event):
-
+        
         if event.key == 'left':
             if self.x_ind == 0:
                 pass
             else:
                 self.x_ind -= 1
+                self.update()
         elif event.key == 'right':
             if self.x_ind == (self.x_max - 1):
                 pass
             else:
                 self.x_ind += 1
+                self.update()
         elif event.key == 'down':
             if self.y_ind == 0:
                 pass
-            else: self.y_ind -= 1
+            else: 
+                self.y_ind -= 1
+                self.update()
         elif event.key == 'up':
             if self.y_ind == (self.y_max - 1):
                 pass
             else:
                 self.y_ind += 1
-        self.update()
+                self.update()
 
 
     def update(self):
-        self.im.set_data(self.data[self.x_ind,self.y_ind,:])
-        title = ('X index = ', self.x_ind, ' Y index = ', self.y_ind)
+        self.ax.clear()
+        self.ax.plot(self.data[self.x_ind,self.y_ind,:])
+        title = 'X index = ' + str(self.x_ind) + '/' + str(self.x_max-1) + '. Y index = ' + str(self.y_ind) + '/' + str(self.y_max-1)
         self.ax.set_title(title)
-        self.im.axes.figure.canvas.draw()
+        self.fig.canvas.draw()
 
 if __name__ == "__main__":
     fig, ax = plt.subplots(1, 1)
@@ -54,7 +58,7 @@ if __name__ == "__main__":
 
     data = np.load(filename)
     print(data.shape)
-    tracker = IndexTracker(ax, data)
+    tracker = IndexTracker(fig, ax, data)
 
     fig.canvas.mpl_connect('key_press_event', tracker.on_key_press)
     plt.show()
