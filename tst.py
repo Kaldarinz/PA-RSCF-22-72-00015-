118292ee0e8382b430a76a10d4eed74957a2700d
Anton Popov <a.popov.fizteh@gmail.com>
2023-11-07 17:52:03 +0300

    some shit

--- tst.py
+++ tst.py
@@ -1,52 +1,11 @@
-import sys
-from PyQt5 import QtWidgets
-import logging
+from dataclasses import dataclass, replace
 
-# Uncomment below for terminal log messages
-# logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(name)s - %(levelname)s - %(message)s')
+@dataclass
+class Tst:
+    a: int = 0
+    b: float = 1.0
 
-class QTextEditLogger(logging.Handler):
-    def __init__(self, parent):
-        super().__init__()
-        self.widget = QtWidgets.QPlainTextEdit(parent)
-        self.widget.setReadOnly(True)
-
-    def emit(self, record):
-        msg = self.format(record)
-        self.widget.appendPlainText(msg)
-
-
-class MyDialog(QtWidgets.QDialog, QtWidgets.QPlainTextEdit):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        logTextBox = QTextEditLogger(self)
-        # You can format what is printed to text box
-        logTextBox.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
-        logging.getLogger().addHandler(logTextBox)
-        # You can control the logging level
-        logging.getLogger().setLevel(logging.DEBUG)
-
-        self._button = QtWidgets.QPushButton(self)
-        self._button.setText('Test Me')
-
-        layout = QtWidgets.QVBoxLayout()
-        # Add the new logging box widget to the layout
-        layout.addWidget(logTextBox.widget)
-        layout.addWidget(self._button)
-        self.setLayout(layout)
-
-        # Connect signal to slot
-        self._button.clicked.connect(self.test)
-
-    def test(self):
-        logging.debug('damn, a bug')
-        logging.info('something to remember')
-        logging.warning('that\'s not right')
-        logging.error('foobar')
-
-app = QtWidgets.QApplication(sys.argv)
-dlg = MyDialog()
-dlg.show()
-dlg.raise_()
-sys.exit(app.exec_())
+dclas = Tst()
+print(dclas)
+dclas = replace(dclas, **{'a': 1, 'b': 3.0})
+print(dclas)
