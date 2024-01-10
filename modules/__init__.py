20cd148ff2a3d0b2ae538f934cb68dac9885c5cb
Anton Popov <a.popov.fizteh@gmail.com>
2024-01-10 16:05:53 +0300

    applicatin registry for pint

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,8 +1,10 @@
 from pint import UnitRegistry
+import pint
 import logging
 
 logger = logging.getLogger(__name__)
 
 ureg = UnitRegistry(auto_reduce_dimensions=True) # type: ignore
+pint.set_application_registry(ureg)
 ureg.default_format = '~.2gP'
 Q_ = ureg.Quantity
